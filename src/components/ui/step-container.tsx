734df265118a768db9006465ca46016145e9b83d
Barbara Peric <b.peric10@gmail.com>
2025-10-08 15:17:11 +0100

    Refactor StepContainer component to use TypeScript props for improved type safety

--- src/components/ui/step-container.tsx
+++ src/components/ui/step-container.tsx
@@ -1,4 +1,8 @@
-function StepContainer({ children }: { children: React.ReactNode }) {
+type StepContainerProps = {
+  children: React.ReactNode
+}
+
+function StepContainer({ children }: StepContainerProps) {
   return <div className="flex flex-col gap-8">{children}</div>
 }
 
