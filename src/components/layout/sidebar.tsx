6901d4184fa64808957e3a9e066ad82f5b01f4a9
Charly Martin <charly@hey.com>
2025-10-13 15:35:27 +0100

    refactor(layout): switch to grid layout for sidebar and header
    Updated layout components to use CSS Grid instead of Flexbox for better structure and responsiveness. Made sidebar and header required props in SidebarLayout, added full width to header, adjusted sidebar styling, and introduced a CSS variable for sidebar height to improve consistency and maintainability.

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarSteps from './sidebar-steps.tsx'
 
 export default function Sidebar() {
   return (
-    <div className="sticky top-10 flex flex-col gap-40 justify-between">
+    <div className="flex flex-col justify-between h-full">
       <SidebarSteps />
       <SidebarLinks />
     </div>
