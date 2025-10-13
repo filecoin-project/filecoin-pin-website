6565a7c99685cd3a3827580cfadfee09b29783ed
Charly Martin <charly@hey.com>
2025-10-13 15:47:46 +0100

    style(layout): adjust padding in sidebar and main sections for improved layout consistency

--- src/components/layout/sidebar-layout.tsx
+++ src/components/layout/sidebar-layout.tsx
@@ -8,10 +8,10 @@ export function SidebarLayout({ children, sidebar, header }: SidebarLayoutProps)
   return (
     <div className="grid grid-cols-[440px_1fr] grid-rows-[auto_1fr] min-h-screen w-full bg-black">
       <div className="col-span-2 h-[var(--spacing-sidebar-height)] border-b border-zinc-800 px-12 flex">{header}</div>
-      <aside className="sticky top-0 self-start h-[calc(100vh-var(--spacing-sidebar-height))] overflow-y-auto p-12">
+      <aside className="sticky top-0 self-start h-[calc(100vh-var(--spacing-sidebar-height))] overflow-y-auto p-12 py-10">
         {sidebar}
       </aside>
-      <main className="overflow-y-auto border-l border-zinc-800 p-12">{children}</main>
+      <main className="overflow-y-auto border-l border-zinc-800 px-15 py-10">{children}</main>
     </div>
   )
 }
