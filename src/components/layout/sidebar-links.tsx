0e84461f5497132513c3ae9a7490083ceb0d1023
Barbara Peric <b.peric10@gmail.com>
2025-10-14 09:23:35 +0100

    refactor(sidebar): centralize sidebar link URLs in constants
    - Introduced a new constants file for sidebar links to improve maintainability.
    - Updated SidebarLinks component to use the new SIDEBAR_LINKS constants for link URLs.

--- src/components/layout/sidebar-links.tsx
+++ src/components/layout/sidebar-links.tsx
@@ -1,21 +1,14 @@
 import { GitBranchIcon, Github, Terminal } from 'lucide-react'
+import { SIDEBAR_LINKS } from '../../constants/sidebar-links.tsx'
 import { LinkIcon } from '../ui/link-icon.tsx'
 import { LinkIconContainer } from '../ui/link-icon-container.tsx'
 
 export default function SidebarLinks() {
   return (
     <LinkIconContainer>
-      <LinkIcon
-        href="https://docs.filecoin.io/about/how-filecoin-works/filecoin-pin/"
-        icon={Github}
-        text="GitHub Action"
-      />
-      <LinkIcon href="https://docs.filecoin.io/about/how-filecoin-works/filecoin-pin/" icon={Terminal} text="CLI" />
-      <LinkIcon
-        href="https://docs.filecoin.io/about/how-filecoin-works/filecoin-pin/"
-        icon={GitBranchIcon}
-        text="Clone this demo"
-      />
+      <LinkIcon href={SIDEBAR_LINKS.GITHUB_ACTION} icon={Github} text="GitHub Action" />
+      <LinkIcon href={SIDEBAR_LINKS.CLI} icon={Terminal} text="CLI" />
+      <LinkIcon href={SIDEBAR_LINKS.CLONE_THIS_DEMO} icon={GitBranchIcon} text="Clone this demo" />
     </LinkIconContainer>
   )
 }
---
+++ src/constants/sidebar-links.tsx
@@ -0,0 +1,5 @@
+export const SIDEBAR_LINKS = {
+  GITHUB_ACTION: 'https://github.com/filecoin-project/filecoin-pin/tree/master/upload-action',
+  CLI: 'https://github.com/filecoin-project/filecoin-pin',
+  CLONE_THIS_DEMO: 'https://github.com/filecoin-project/filecoin-pin-website/fork',
+}
