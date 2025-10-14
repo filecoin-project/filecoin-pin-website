700e3b6506f81b6baee9e76c6f626a2262fa9780
Charly Martin <charly@hey.com>
2025-10-14 14:31:56 +0100

    style(constants): add trailing comma to PROOF_BUTTON URL for consistency

--- src/constants/upload-completed-links.ts
+++ src/constants/upload-completed-links.ts
@@ -3,5 +3,5 @@ export const UPLOAD_COMPLETED_LINKS = {
   IPFS_CID_DOWNLOAD: 'https://dweb.link/ipfs/',
   FILECOIN_PIECE_CID_DOWNLOAD: '',
   PROVIDER_LINK: 'https://pdp.vxb.ai/calibration/providers/',
-  PROOF_BUTTON: 'https://pdp.vxb.ai/calibration/dataset/'
+  PROOF_BUTTON: 'https://pdp.vxb.ai/calibration/dataset/',
 }
