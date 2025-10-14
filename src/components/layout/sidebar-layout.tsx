f041a75a2763ada761c2a11bbf585ed0aec9bc6f
Charly Martin <charly@hey.com>
2025-10-14 18:49:12 +0100

    feat: add company logo SVG to public directory
    Added a new SVG logo file containing white paths for branding elements, likely for display on the website or application interface.

--- src/components/layout/sidebar-layout.tsx
+++ src/components/layout/sidebar-layout.tsx
@@ -1,3 +1,5 @@
+import { Logo } from '../ui/logo.tsx'
+
 interface SidebarLayoutProps {
   children: React.ReactNode
   sidebar: React.ReactNode
@@ -7,13 +9,21 @@ interface SidebarLayoutProps {
 export function SidebarLayout({ children, sidebar, header }: SidebarLayoutProps) {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[440px_1fr] grid-rows-[auto_1fr] min-h-screen w-full bg-black">
-      <div className="col-span-2 h-[var(--spacing-sidebar-height)] border-b border-zinc-800 px-10 lg:px-12 flex">
+      {/* Header */}
+      <div className="col-span-2 h-[var(--spacing-sidebar-height)] border-b border-zinc-800 px-12 md:flex hidden">
         {header}
       </div>
+      <div className="col-span-2 border-b py-4 border-zinc-800 px-10 md:hidden flex">
+        <Logo />
+      </div>
+
+      {/* Sidebar */}
       <aside className="hidden sticky top-0 self-start h-[calc(100vh-var(--spacing-sidebar-height))] overflow-y-auto p-12 py-10 lg:block">
         {sidebar}
       </aside>
-      <main className="overflow-y-auto border-l border-zinc-800 px-10 lg:px-15 py-10">{children}</main>
+
+      {/* Main content */}
+      <main className="overflow-y-auto lg:border-l lg:border-zinc-800 px-10 lg:px-15 py-10">{children}</main>
     </div>
   )
 }
